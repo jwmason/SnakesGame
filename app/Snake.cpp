f6d89830998d168d3c1865f0b26d3f3ee7019467
Mason Wong <masonjw1@uci.edu>
2023-10-17 14:51:38 -0700

    Implemented Snake::move() function

--- app/Snake.cpp
+++ app/Snake.cpp
@@ -39,6 +39,28 @@ bool Snake::isDead() const
 void Snake::move()
 {
     // Attempt a move in a random direction;  if it can't move, don't move.
+    // Generate a random direction
+    int direction = rand() % 4; // % 4 so it can be 0, 1, 2, or 3 (4 directions)
+
+    // Depending on direction, move the Snake position. Check also if it can even move
+    // 0 - up, 1 - left, 2 - down, 3 - right
+    // Access the Plane row/col the Snake is on, and check that it can move
+    if (direction == 0 && m_col < onWhichPlane->cols())
+    {
+        m_col += 1; // move up
+    }
+    else if (direction == 1 && m_row > 1)
+    {
+        m_row -= 1; // move left
+    }
+    else if (direction == 2 && m_col > 1)
+    {
+        m_col -= 1; // move down
+    }
+    else if (direction == 3 && m_row < onWhichPlane->rows())
+    {
+        m_row += 1; // move right
+    }
 }
 
 void Snake::setDead()
